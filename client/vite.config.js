581dbeec2e44f3d1a78ef0c0fb9d3fce4b51b3e5
codingcreedd <moubayedmarwancoding@gmail.com>
2024-09-30 10:56:53 +0300

    fix 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.html"],
-  base: "./",
+  base: "https://messageapp-one.vercel.app/src/main.jsx",
 });
