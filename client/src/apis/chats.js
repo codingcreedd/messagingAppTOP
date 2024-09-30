456d2bb1784088a918211ac209b3462703265a1c
codingcreedd <moubayedmarwancoding@gmail.com>
2024-09-30 10:48:51 +0300

    update baseurl

--- client/src/apis/chats.js
+++ client/src/apis/chats.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3001/chats',
+    baseURL: '/chats',
     withCredentials: true,
     validateStatus: (status) => {
         return true;
--- client/src/apis/user.js
+++ client/src/apis/user.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:3001/user',
+    baseURL: '/user',
     withCredentials: true,
     validateStatus: (status) => {
         return true;
