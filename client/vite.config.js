9f367a4aa9b8938ca6b93f7955844af219c45ac3
codingcreedd <moubayedmarwancoding@gmail.com>
2024-09-30 11:44:27 +0300

    fix 8

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://messageapp-one.vercel.app/src/main.jsx",
+  base: "https://whatsup-bice.vercel.app/src/main.jsx",
 });
