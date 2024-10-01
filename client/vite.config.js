c5eb3c8942c37108e63042f893cef5de57e219f2
codingcreedd <moubayedmarwancoding@gmail.com>
2024-10-01 15:25:07 +0300

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://whatsuptop.netlify.app/src/main.jsx",
+  base: "src/main.jsx",
 });
