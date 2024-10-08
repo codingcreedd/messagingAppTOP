a5c1ae870d6b82c32b0a2594b0aa64f83dcb1d37
codingcreedd <moubayedmarwancoding@gmail.com>
2024-10-09 00:10:05 +0300

    fix origin

--- server/app.js
+++ server/app.js
@@ -7,14 +7,14 @@ const cors = require('cors');
 
 //middlewares
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://whatsuptop.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://whatsuptop.netlify.app');
   res.header('Access-Control-Allow-Credentials', true);
   next();
 });
